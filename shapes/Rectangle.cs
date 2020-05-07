e32207f51f0e1937353056a93945ba128fb2f49f
ol222hf <ol222hf@student.lnu.se>
2020-05-08 00:15:46 +0200

    Created empty getters for Area and Perimeter

--- shapes/Rectangle.cs
+++ shapes/Rectangle.cs
@@ -2,7 +2,16 @@ namespace examination_2
 {
     public class Rectangle : Shape2D
     {
-
-
+        public Rectangle(double length, double width)
+        : base(ShapeType.Rectangle, length, width)
+        { }
+        public override double Area
+        {
+            get;
+        }
+        public override double Perimeter
+        {
+            get;
+        }
     }
 }
