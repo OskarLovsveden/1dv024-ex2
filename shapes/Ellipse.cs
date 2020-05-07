c422212327effbad071eaf15f8ae55283e1dda24
ol222hf <ol222hf@student.lnu.se>
2020-05-08 00:02:08 +0200

    Added calculations for Area and Perimeter

--- shapes/Ellipse.cs
+++ shapes/Ellipse.cs
@@ -1,8 +1,21 @@
+using System;
+
 namespace examination_2
 {
     public class Ellipse : Shape2D
     {
+        public Ellipse(double diameter)
+        : base(ShapeType.Ellipse, diameter / 2, diameter / 2)
+        {
 
+        }
+        public Ellipse(double hdiameter, double vdiameter)
+        : base(ShapeType.Ellipse, hdiameter / 2, vdiameter / 2)
+        {
 
+        }
+
+        public override double Area { get => Math.PI * (Length * Width); }
+        public override double Perimeter { get => 2 * Math.PI * Math.Sqrt((Length * Length + Width * Width) / 2); }
     }
 }
