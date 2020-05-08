f2d1422c65df7d7e4228823e1cd0e26fd7791999
ol222hf <ol222hf@student.lnu.se>
2020-05-08 12:23:20 +0200

    Cuboid passing values through base properly

---
+++ shapes/Cuboid.cs
@@ -0,0 +1,10 @@
+namespace examination_2
+{
+    public class Cuboid : Shape3D
+    {
+
+        public Cuboid(double length, double width, double height)
+        : base(ShapeType.Cuboid, new Rectangle(length, width), height)
+        { }
+    }
+}
