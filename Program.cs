251a03430c4d060430b97bb12f568636368944ad
ol222hf <ol222hf@student.lnu.se>
2020-05-20 12:16:07 +0200

    Calling Method to run application

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace examination_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Application.Run();
         }
     }
 }
